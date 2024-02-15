6e9a5939cc89cb219e2ee0808b7f50c309748136
Cocodom <156429033+Cocodom@users.noreply.github.com>
2024-02-15 14:13:15 +0100

    Graph now exists

--- Python/graph.py
+++ Python/graph.py
@@ -27,6 +27,34 @@ except ModuleNotFoundError as e:
     print("le module PySide6 devrait être installé pour que ce programme puisse fonctionner, lisez README.md pour plus de détails", file=stderr)
     raise e
 
+
+class Graph :
+    """Airports link"""
+    
+    def __init__(self, width) :
+        self.values = [[0 for j in range(width)] for i in range(width)]
+        self.n_airports = width
+    
+    
+    def new_airport(self) :
+        """None --> Graph
+        Adds a new Airport to the Graph"""
+        
+        self.n_airports += 1
+        self.values.append([0 for i in range(self.n_airports)])
+        for i in range(self.n_airports-1) :
+            self.values[i].append(0)
+    
+    
+    def new_link(self, airport1, airport2, cost) :
+        """Airport_Id, Airport_Id, Int --> Graph
+        Adds a new link of weight cost between Airport1 and Airport2"""
+        
+        if self.values[airport1][airport2] == 0 :
+            self.values[airport1][airport2] = cost
+            self.values[airport2][airport1] = cost
+
+
 #on identifie les aéroports par les noeuds du graphe, en les modélisant par une matrice.
 nom_noeud_graph = [] #on stockera les initiales des aéroports dans une list de str.
 noeud_graph = [] #matrice référençant les liaisons entre aéroports, 
