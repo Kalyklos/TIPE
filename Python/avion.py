1bf3facb3b970a9501b0e150ae42016a91ef71b9
Cocodom <156429033+Cocodom@users.noreply.github.com>
2024-02-15 14:11:21 +0100

    Init exists

--- Python/avion.py
+++ Python/avion.py
@@ -9,3 +9,16 @@ from typing import *
 from math import *
 
 from . import settings
+
+class Plane :
+    """C'EST DES NAVIONS ILS FONT VROOM VROOOOOOOOOM"""
+    
+    def __init__(self, fuel_capacity, fuel_consumption, etops, position) :
+        assert type(fuel_capacity) == float, "fuel_capacity has to be of type float"
+        assert type(fuel_consumption) == float, "fuel_consumption has to be an float"
+        assert type(etops) == int, "etops has to be an integer"
+        
+        self.fuel_cap = fuel_capacity
+        self.fuel_cons = fuel_consumption
+        self.etops = etops
+        self.pos = position
