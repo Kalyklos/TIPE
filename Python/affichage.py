5d8978b3bef9aeb90544a514f66489038cb881f3
Kalyklos <matthebg73@gmail.com>
2024-03-07 15:31:58 +0100

    tkt

--- Python/affichage.py
+++ Python/affichage.py
@@ -11,8 +11,6 @@ try:
     from PySide6.QtCore import *
     from PySide6.QtWidgets import *
     from PySide6.QtGui import *
-    from random import *
-
     Signal()
 except ModuleNotFoundError as e:
     print("le module PySide6 devrait être installé pour que ce programme puisse fonctionner, lisez README.md pour plus de détails", file=stderr)
