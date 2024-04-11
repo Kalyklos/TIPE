099a579da80fac460a4eb8836bd85a184fb56d6b
Cocodom <coco281242@gmail.com>
2024-04-11 15:24:07 +0200

    popopo

--- Python/avion.py
+++ Python/avion.py
@@ -10,6 +10,7 @@ from math import *
 from numpy import *
 
 from settings import *
+from graph import *
 ledictionnairedesetops = {}
 
 class Plane :
@@ -22,7 +23,7 @@ class Plane :
                | fuel_consumption of type float (kg.s^-1)
                | etops of type int
                | position of type Airport*Airport*x∈[0;1]
-               | speed of type int"""
+               | speed of type int (m.s^-1)"""
         self.fuel_cap = fuel_capacity
         self.fuel = fuel_capacity
         self.fuel_cons = fuel_consumption
@@ -59,15 +60,19 @@ class Plane :
         if self.etops >= ledictionnairedesetops[route] : #to do : créer ledictonnairedesetops
             return True
         return False
-    def fuel_alert(self) :
-        if self.etops > self.flight_time_left() :
-            return False
-        return True
     def remove_s_fuel(self) :
+        """removes 1s worth of fuel
+        """
         self.fuel = self.fuel-self.fuel_cons
     def remove_min_fuel(self) :
+        """removes 1min worth of fuel
+        """
         self.fuel = self.fuel-60*self.fuel_cons
     def remove_h_fuel(self) :
+        """removes 1h worth of fuel
+        """
         self.fuel = self.fuel-3600*self.fuel_cons
     def tick(self) :
-        pass
+        """simulates one tick for the plane
+        """
+        newtruc = self.speed*60/graph[self.pos[0]][self.pos[1]]
