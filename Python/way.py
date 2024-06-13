ced05cb68e4baa4f9a0ab92239081c63dc9356b0
Kalyklos <matthebg73@gmail.com>
2024-06-13 13:56:37 +0200

    iuuo

--- Python/way.py
+++ Python/way.py
@@ -135,82 +135,35 @@ class Graph():
     def __init__(self, V):
         self.V = V
         self.graph = defaultdict(list)
- 
-    # Adds an edge to an undirected graph
     def addEdge(self, src, dest, weight):
- 
-        # Add an edge from src to dest.  A new node 
-        # is added to the adjacency list of src. The 
-        # node is added at the beginning. The first 
-        # element of the node has the destination 
-        # and the second elements has the weight
         newNode = [dest, weight]
         self.graph[src].insert(0, newNode)
- 
-        # Since graph is undirected, add an edge 
-        # from dest to src also
         newNode = [src, weight]
         self.graph[dest].insert(0, newNode)
- 
-    # The main function that calculates distances 
-    # of shortest paths from src to all vertices. 
-    # It is a O(ELogV) function
     def dijkstra(self, src):
- 
-        V = self.V  # Get the number of vertices in graph
-        dist = []   # dist values used to pick minimum 
-                    # weight edge in cut
- 
-        # minHeap represents set E
+        V = self.V 
+        dist = [] 
         minHeap = Heap()
- 
-        #  Initialize min heap with all vertices. 
-        # dist value of all vertices
         for v in range(V):
             dist.append(1e7)
-            minHeap.array.append( minHeap.
-                                newMinHeapNode(v, dist[v]))
+            minHeap.array.append( minHeap.newMinHeapNode(v, dist[v]))
             minHeap.pos.append(v)
- 
-        # Make dist value of src vertex as 0 so 
-        # that it is extracted first
         minHeap.pos[src] = src
         dist[src] = 0
         minHeap.decreaseKey(src, dist[src])
- 
-        # Initially size of min heap is equal to V
         minHeap.size = V
- 
-        # In the following loop, 
-        # min heap contains all nodes
-        # whose shortest distance is not yet finalized.
         while minHeap.isEmpty() == False:
- 
-            # Extract the vertex 
-            # with minimum distance value
             newHeapNode = minHeap.extractMin()
             u = newHeapNode[0]
- 
-            # Traverse through all adjacent vertices of 
-            # u (the extracted vertex) and update their 
-            # distance values
             for pCrawl in self.graph[u]:
- 
                 v = pCrawl[0]
- 
-                # If shortest distance to v is not finalized 
-                # yet, and distance to v through u is less 
-                # than its previously calculated distance
                 if (minHeap.isInMinHeap(v) and
                      dist[u] != 1e7 and \
                    pCrawl[1] + dist[u] < dist[v]):
                         dist[v] = pCrawl[1] + dist[u]
- 
-                        # update distance value 
-                        # in min heap also
                         minHeap.decreaseKey(v, dist[v])
  
-       # printArr(dist,V)
+            printArr(dist,V)
 
 class Graph_list:
     def __init__(self, size):
